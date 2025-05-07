3e679343a86978dda8a21485ff62b2bac3e719e1
Dev546323 <devhirawat18@gmail.com>
2025-05-07 16:24:52 +0530

    modified:   extra/time.cpp

--- extra/time.cpp
+++ extra/time.cpp
@@ -24,8 +24,55 @@ void constant(int &a, int &b){
 //time complexity irrespective of input size. 
 
 
+//O(logn) this is a good time complexity
+//it is very close to constant complexity but considered very good.
+//binary search 
 
 
+//O(nlogn) less than n^2 decent enough complexity
+//optimized sort algos use this complexity. 
+
+
+
+
+//quadratic time complexity. 
+//loop in a loop increases time complexity n^number of loop in loop
+vector<int> quadratic (vector<int> arr, int target){
+    vector<int> returna;
+    int size = arr.size()-1;
+    for(int i=0;i<=size;i++){
+        for(int j=i+1;j<=size;j++){
+            if(i==j) continue;
+            if(arr[i]+arr[j]==target){
+                returna.push_back(i);
+                returna.push_back(j);
+            }
+        }
+    }
+    return returna;
+}
+//not a very good time complexity as it increases quadratically with the input size. 
+
+
+//cubic time complexity. 
+//really not good. 
+void cubic(int a[], int b){
+    for(int i=0;i<b;i++){ //array from start to end
+  
+        for(int j=i;j<b;j++){ //array from the index to remaining
+            int sum=0;
+           
+            for(int k=i;k<=j;k++){ // between index and remaining 
+                cout<<a[k];
+                sum+=a[k];
+            
+            }
+            cout<<"-"<<sum<<"  ";
+        }
+        cout<<endl;
+    }
+
+}
 
 
 
