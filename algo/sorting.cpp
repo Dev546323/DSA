1862d4e924df8058076774e68132582f20a3cde9
Dev546323 <devhirawat18@gmail.com>
2025-05-14 16:42:42 +0530

    new file:   .vscode/settings.json 	modified:   algo/sorting 	modified:   algo/sorting.cpp

--- algo/sorting.cpp
+++ algo/sorting.cpp
@@ -19,9 +19,9 @@ void bubble(vector<int> arr){ //n^2 time complexity.
     for(int i=0;i<arr.size();i++){
 
         bool iswap = false;
-        for(int j=0;j<arr.size()-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+        for(int j=0;j<arr.size()-i-1;j++){ 
+            if(arr[j]>arr[j+1]){ //checks if the lower index has a higher value, 
+                swap(arr[j],arr[j+1]);//in which case it swaps and puts the higher value at a later index
                 iswap = true;
             }
         }
@@ -32,7 +32,23 @@ void bubble(vector<int> arr){ //n^2 time complexity.
    // print(arr);
 }
 
+void selection(vector<int> arr){
 
+    for(int i=0;i<arr.size();i++){
+        int small = i;  //considering the current smallest element to be the ith element
+
+        for(int j=i+1;j<arr.size();j++){
+           if(arr[j]<arr[small]){ //check if smallest element is smaller than a[j]
+           small=j;   //changing smallest element if j is actually the smallest
+  
+        }
+        }
+      
+        swap(arr[i],arr[small]); //changing i to actually be the smallest element 
+    }
+    //print(arr);
+
+}
 
 
 
@@ -50,44 +66,52 @@ auto duration = duration_cast<microseconds>(end - start);
 cout << "Time taken: " << duration.count() << " microseconds" << endl;
 
 // 🧼 Test Case 2: Reverse Sorted
-// Input: [5, 4, 3, 2, 1]
-// Expected Output: 1 2 3 4 5
-bubble({5, 4, 3, 2, 1});
+vector<int> test2 = {5, 4, 3, 2, 1};
+bubble(test2);
+selection(test2);
 
 // 🧼 Test Case 3: All Elements Same
-// Input: [7, 7, 7, 7, 7]
-// Expected Output: 7 7 7 7 7
-bubble({7, 7, 7, 7, 7});
+vector<int> test3 = {7, 7, 7, 7, 7};
+bubble(test3);
+selection(test3);
+
 
 // 🧼 Test Case 4: Random Order
-// Input: [3, 1, 4, 2, 5]
-// Expected Output: 1 2 3 4 5
-bubble({3, 1, 4, 2, 5});
+vector<int> test4 = {3, 1, 4, 2, 5};
+bubble(test4);
+selection(test4);
+
 
 // 🧼 Test Case 5: Negative Numbers
-// Input: [-3, -1, -4, -2, -5]
-// Expected Output: -5 -4 -3 -2 -1
-bubble({-3, -1, -4, -2, -5});
+vector<int> test5 = {-3, -1, -4, -2, -5};
+bubble(test5);
+selection(test5);
+
 
 // 🧼 Test Case 6: Single Element
-// Input: [9]
-// Expected Output: 9
-bubble({9});
+vector<int> test6 = {9};
+bubble(test6);
+selection(test6);
+
 
 // 🧼 Test Case 7: Two Elements Sorted
-// Input: [1, 2]
-// Expected Output: 1 2
-bubble({1, 2});
+vector<int> test7 = {1, 2};
+bubble(test7);
+selection(test7);
+
 
 // 🧼 Test Case 8: Two Elements Unsorted
-// Input: [2, 1]
-// Expected Output: 1 2
-bubble({2, 1});
+vector<int> test8 = {2, 1};
+bubble(test8);
+selection(test8);
+
 
 // 🧼 Test Case 9: Large Range
-// Input: [100, 2000, 30, 400, 500]
-// Expected Output: 30 100 400 500 2000
-bubble({100, 2000, 30, 400, 500});
+vector<int> test9 = {100, 2000, 30, 400, 500};
+bubble(test9);
+selection(test9);
+
+
 
 // 🧼 Test Case 10: Empty Array
 // Input: []
@@ -104,15 +128,26 @@ bubble(stress1);
 end = high_resolution_clock::now();
 duration = duration_cast<microseconds>(end - start);
 cout << "Time taken: " << duration.count() << " microseconds" << endl;
+cout<<"Selection"<<endl;
+start = high_resolution_clock::now();
+selection(stress1);
+end = high_resolution_clock::now();
+
 
 
 // 🧨 Stress 2: 5,000 elements in random order
 // You might finish your degree before this finishes
 vector<int> stress2(500);
 generate(stress2.begin(), stress2.end(), rand);
-cout<<endl<<"Stress2"<<endl<<endl;
+cout<<endl<<"Stress2"<<endl;
 start = high_resolution_clock::now();
 bubble(stress2);
+end = high_resolution_clock::now();
+duration = duration_cast<microseconds>(end - start);
+cout << "Time taken: " << duration.count() << " microseconds" << endl;
+cout<<"Selection"<<endl;
+start = high_resolution_clock::now();
+selection(stress2);
 end = high_resolution_clock::now();
 duration = duration_cast<microseconds>(end - start);
 cout << "Time taken: " << duration.count() << " microseconds" << endl;
@@ -121,37 +156,52 @@ cout << "Time taken: " << duration.count() << " microseconds" << endl;
 // 🧊 Stress 3: 1,000 elements, all same
 // Should be quick... unless your bubble sort is on vacation
 vector<int> stress3(1000, 42);
-cout<<endl<<"Stress3"<<endl<<endl;
+cout<<endl<<"Stress3"<<endl;
 start = high_resolution_clock::now();
 bubble(stress3);
 end = high_resolution_clock::now();
 duration = duration_cast<microseconds>(end - start);
 cout << "Time taken: " << duration.count() << " microseconds" << endl;
-
+cout<<"Selection"<<endl;
+start = high_resolution_clock::now();
+selection(stress3);
+end = high_resolution_clock::now();
+duration = duration_cast<microseconds>(end - start);
+cout << "Time taken: " << duration.count() << " microseconds" << endl;
 
 // 🔥 Stress 4: Already sorted 10,000 elements
 // Best-case scenario... but bubble sort still bubbles slowly
 vector<int> stress4(1000);
 iota(stress4.begin(), stress4.end(), 1);
-cout<<endl<<"Stress4"<<endl<<endl;
+cout<<endl<<"Stress4"<<endl;
 start = high_resolution_clock::now();
 bubble(stress4);
 end = high_resolution_clock::now();
 duration = duration_cast<microseconds>(end - start);
 cout << "Time taken: " << duration.count() << " microseconds" << endl;
-
+cout<<"Selection"<<endl;
+start = high_resolution_clock::now();
+selection(stress4);
+end = high_resolution_clock::now();
+duration = duration_cast<microseconds>(end - start);
+cout << "Time taken: " << duration.count() << " microseconds" << endl;
 
 // 💣 Stress 5: Descending 10,000 elements (max torture)
 // Because who *doesn't* love nested loops?
 vector<int> stress5(1000);
 iota(stress5.rbegin(), stress5.rend(), 1);
-cout<<endl<<"Stress5"<<endl<<endl;
+cout<<endl<<"Stress5"<<endl;
 start = high_resolution_clock::now();
 bubble(stress5);
 end = high_resolution_clock::now();
 duration = duration_cast<microseconds>(end - start);
 cout << "Time taken: " << duration.count() << " microseconds" << endl;
-
+cout<<"Selection"<<endl;
+start = high_resolution_clock::now();
+selection(stress5);
+end = high_resolution_clock::now();
+duration = duration_cast<microseconds>(end - start);
+cout << "Time taken: " << duration.count() << " microseconds" << endl;
 
 
 }
