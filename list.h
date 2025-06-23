80d3210e6163e516b3a79c3688572bc68ac332f3
Dev546323 <devhirawat18@gmail.com>
2025-06-24 02:35:51 +0530

    modified:   list.h 	modified:   tempCodeRunnerFile 	modified:   tempCodeRunnerFile.cpp

--- list.h
+++ list.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "/home/yestodrugs/Desktop/DSA/print.h"
 #include <iostream>
 #include<vector>
 #include<list>
@@ -11,10 +10,12 @@ struct Node{
 
     T data;
     Node* next;
+    Node* prev; 
 
     Node(T val) {
         data = val;
         next = nullptr;
+        prev = nullptr;
     }
 };
 
@@ -113,17 +114,20 @@ void pop_back(){
         }
         cout<<endl;
     }
-    int search(T val){
+
+
+    void search(T val){
     if(head == tail or head == nullptr){
         cout<<"Please die\n";
+        return;
     }
 
     Node<T>* current = head;
     int index =0; 
     while(current != nullptr){
         if(current->data == val){
-            // cout << "Value " << val << " found at index: " << index << endl;
-            return index;
+            cout << "Value " << val << " found at index: " << index << endl;
+            return;
         }
         current = current->next; // Move to the next node.
         index++;
@@ -132,4 +136,132 @@ void pop_back(){
 }
 
 };
+
+template<typename T>
+struct doublylinkedlist {
+    Node<T>* head;
+    Node<T>* tail;
+
+    doublylinkedlist() {
+        head = tail = nullptr;
+    }
+    doublylinkedlist(Node<T>* node) {
+        head = node;
+    }
+
+    void insert(T val, int pos){
+    Node<T>* newnode = new Node(val);
+    if(pos == 0){
+        push_front(val); 
+        return;
+    }
+    Node<T>* current = head;
+    while(current != nullptr && pos > 1){
+        current = current->next;
+        pos--;
+    }
+    if(current==nullptr){
+        cout<< "Please enter a valid position." << endl;
+        return;
+    }
+    else if(current == tail){
+        push_back(val); // If the position is at the end, we can use push_back.
+        return;
+    }
+    else {
+         newnode->prev = current;
+       newnode->next = current->next; // The new node's next will point to the current node's next.
+       current->next = newnode; // The current node's next will now point to the new node.
+         if(newnode->next != nullptr) {
+              newnode->next->prev = newnode; 
+         }
+     }
+    }   
+        void push_front(T val) { 
+        Node<T>* newnode = new Node<T>(val); 
+        if (head == nullptr) {
+            head = tail = newnode; 
+        } else {
+            newnode->next = head;  
+             head->prev = newnode; 
+             newnode->prev = nullptr; // The new node's prev should be nullptr since it's the new head.
+            head = newnode; 
+        }
+    }
+    void push_back(T val){
+    Node<T>* newnode = new Node(val); 
+    if (head == nullptr) {
+        head = tail = newnode; 
+     } else {
+        tail->next = newnode; //The current tail points to the new node. 
+            newnode->prev = tail; // The new node's prev should point to the current tail.
+            newnode->next = nullptr; // The new node's next should be nullptr since it's the new tail.
+        tail = newnode; // the newnode becomes the new tail.
+
+         }
+    }
+
+    void pop_front(){ 
+    if (head == nullptr) {
+        cout << "List is empty, nothing to pop." << endl;
+        return;
+    }
+    Node<T>* temp = head; 
+    head = head->next; 
+    if (head != nullptr) { // If the list is not empty after popping
+        head->prev = nullptr; // Set the new head's prev to nullptr
+    } else {
+        tail = nullptr; // If the list is now empty, set tail to nullptr
+    }
+    delete temp; // Delete the old head
+    
+}
+
+void pop_back(){
+     if (head == nullptr) {
+        cout << "List is empty, nothing to pop." << endl;
+        return;
+    }
+    if (head == tail) { // If there's only one node
+        delete head;
+        head = tail = nullptr; 
+        return;
+    }
+    Node<T>* temp = tail;
+   tail = tail->prev; 
+    delete temp; 
+    tail->next = nullptr; 
+}
+
+
+    void print() { 
+        Node<T>* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next; 
+        }
+        cout<<endl;
+    }
+
+
+    void search(T val){
+    if(head == tail or head == nullptr){
+        cout<<"Please die\n";
+        return;
+    }
+
+    Node<T>* current = head;
+    int index =0; 
+    while(current != nullptr){
+        if(current->data == val){
+            cout << "Value " << val << " found at index: " << index << endl;
+            return;
+        }
+        current = current->next; // Move to the next node.
+        index++;
+    }
+    cout << "Value " << val << " not found in the list." << endl;
+}
+};
+
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,49 @@
 #include<iostream>
+#include "/home/yestodrugs/Desktop/DSA/list.h"
+#include<string>
+
 using namespace std; 
 
 int main(){
-    cout<<(int)'9'<<endl;
+     cout << "=== Integer DoubleLinkedList ===" << endl;
+    doublylinkedlist<int> list1;
+    list1.push_back(10);
+    list1.push_back(20);
+    list1.push_front(5);
+    list1.insert(15, 2); // insert 15 at position 2
+    list1.print();       // Expected: 5 10 15 20
+    list1.pop_front();   // Removes 5
+    list1.pop_back();    // Removes 20
+    list1.print();       // Expected: 10 15
+    cout << "Search for 15: ";
+     list1.search(15)  ;
+    cout << "Search for 5: ";
+     list1.search(5)  ;
 
-    for(int i=48;i<123;i++){
-        cout<<" "<<(char)i;
-    }
+    cout << "\n=== Char DoubleLinkedList ===" << endl;
+    doublylinkedlist<char> list2;
+    list2.push_back('a');
+    list2.push_back('c');
+    list2.insert('b', 1); // insert 'b' between a and c
+    list2.print();        // Expected: a b c
+    list2.pop_front();    // Removes a
+    list2.pop_back();     // Removes c
+    list2.print();        // Expected: b
+    cout << "Search for b: " ;
+    list2.search('b')  ;
+    cout << "Search for a: ";
+     list2.search('a')  ;
+
+    cout << "\n=== String DoubleLinkedList ===" << endl;
+    doublylinkedlist<string> list3;
+    list3.push_back("hello");
+    list3.push_front("world");
+    list3.insert("dear", 1); // insert "dear" between world and hello
+    list3.print();           // Expected: world dear hello
+    list3.pop_back();        // Removes hello
+    list3.print();           // Expected: world dear
+    cout << "Search for dear: ";
+     list3.search("dear") ;
+    cout << "Search for hello: ";
+     list3.search("hello") ;
 }
